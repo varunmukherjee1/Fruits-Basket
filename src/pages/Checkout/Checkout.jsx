a8a271fd48942407edb9c985d307f0aca926a798
Varun <varunmukherjee7@gmail.com>
2022-11-12 21:47:23 +0530

    final

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -23,41 +23,43 @@ function Checkout() {
 
   return (
     <div className = {classes.checkout}>
-      {overlay && 
-        <>
-          <div className = {classes.overlay}></div>
-          <div className = {classes.model}>
-            Order Placed Successfully ✅
-          </div>
-        </>
-      }
-      {(ctx.items.length === 0) && <div className = {classes.notif}>
-        <p>Your cart in Empty !</p>
-        <p>Add some products from <Link to = "/">here</Link> 👈🏻</p>
-        <div className = {classes.line}></div>
-      </div>}
-      {!(ctx.items.length === 0) && 
-        <>
-          <div className = {classes.cart}>
-            {
-              ctx.items.map((item) => (<CartItem key = {item.id} obj = {item}/>))
-            }
-          </div>
-          <div className = {classes.preview}>
-            <h2>Summary</h2>
-            <div className = {classes.line}></div>
-
-            <p>Total Items :  <strong>{ctx.items.reduce((acc,item) => (acc + item.amount),0)}</strong></p>
-            <p>Sub Total (💵):  <strong>Rs. {ctx.totalAmount}</strong></p>
-
-            <div className = {classes.line}></div>
-
-            <div>
-              <button onClick = {placeOrder}>Place Order</button>
+      <div className = {classes.parent}>
+        {overlay && 
+          <>
+            <div className = {classes.overlay}></div>
+            <div className = {classes.model}>
+              Order Placed Successfully ✅
             </div>
-          </div>
-        </>
-      }
+          </>
+        }
+        {(ctx.items.length === 0) && <div className = {classes.notif}>
+          <p>Your cart in Empty !</p>
+          <p>Add some products from <Link to = "/">here</Link> 👈🏻</p>
+          <div className = {classes.line}></div>
+        </div>}
+        {!(ctx.items.length === 0) && 
+          <>
+            <div className = {classes.cart}>
+              {
+                ctx.items.map((item) => (<CartItem key = {item.id} obj = {item}/>))
+              }
+            </div>
+            <div className = {classes.preview}>
+              <h2>Summary</h2>
+              <div className = {classes.line}></div>
+
+              <p>Total Items :  <strong>{ctx.items.reduce((acc,item) => (acc + item.amount),0)}</strong></p>
+              <p>Sub Total (💵):  <strong>Rs. {ctx.totalAmount}</strong></p>
+
+              <div className = {classes.line}></div>
+
+              <div>
+                <button onClick = {placeOrder}>Place Order</button>
+              </div>
+            </div>
+          </>
+        }
+      </div>
     </div>
   )
 }
